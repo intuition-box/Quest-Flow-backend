2e523fa9a3dc5d030895aa3c05d5a4b95dc2485e
cakezero <sareahj.com@gmail.com>
2025-10-28 02:49:06 +0100

    server structure

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { type Response, type Request } from "express";
+import cors from "cors";
+import { port } from "@/utils/env";
+import DB from "@/config/db";
+import logger from "@/config/logger";
+import appRoutes from "@/routes/appRoutes";
+import helmet from "helmet";
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.get("/api", appRoutes);
+
+server.listen(port, async () => {
+	await DB();
+	logger.info(`Server is running on port ${port}`);
+});
---
+++ src/controllers/app.controller.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+
+export const home = async (req: Request, res: Response) => {
+  res.send("hi!")
+}
