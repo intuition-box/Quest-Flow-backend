4c3266814a14397ac2545015316a9360fde18190
cakezero <sareahj.com@gmail.com>
2025-11-10 07:00:55 +0100

    chore: project sign up

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,36 +1,62 @@
 import cryptoRandomString from "crypto-random-string";
 import logger from "@/config/logger";
-import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR } from "@/utils/status.utils";
+import {
+	BAD_REQUEST,
+	CREATED,
+	INTERNAL_SERVER_ERROR,
+} from "@/utils/status.utils";
 import { formatDate } from "date-fns";
 import { user } from "@/models/user.model";
+import { getRefreshToken, JWT } from "@/utils/utils";
+import { project } from "@/models/projects.model";
 
 export const signUp = async (req: GlobalRequest, res: GlobalResponse) => {
 	try {
-    const { username, referrer }: { username: string, referrer?: string } = req.body;
+		const { username, referrer }: { username: string; referrer?: string } =
+			req.body;
 
-    if (!username || username.length < 4) {
-      res.status(BAD_REQUEST).json({ error: "username cannot be empty or less than 4 characters" });
-      return;
-    }
+		if (!username || username.length < 4) {
+			res
+				.status(BAD_REQUEST)
+				.json({ error: "username cannot be empty or less than 4 characters" });
+			return;
+		}
 
-    const referrerCode = cryptoRandomString({ length: 8, type: "alphanumeric" });
+		const referrerCode = cryptoRandomString({
+			length: 8,
+			type: "alphanumeric",
+		});
 
-    const dateJoined = formatDate(new Date(), "MMM, y");
+		const dateJoined = formatDate(new Date(), "MMM, y");
 
-    const referral = {
-      code: referrerCode
-    }
+		const referral = {
+			code: referrerCode,
+		};
 
-    const userReferrer = await user.findOne({ referral: { code: referrer } });
+		const userReferrer = await user.findOne({ referral: { code: referrer } });
 
-    if (!userReferrer) {
-      await user.create({ username, referral, dateJoined });
-    } else {
+    const newUser = new user({ username, referral, dateJoined });
+
+		if (userReferrer) {
       await userReferrer.updateOne({ $inc: { xp: 10, "referral.xp": 10 } });
-      await user.create({ username, referral, dateJoined, xp: 10 });
-    }
+      newUser.xp = 10;
+			await newUser.save();
+		}
 
-    res.status(CREATED).json({ message: "user created!" })
+		const id = newUser._id;
+
+		const accessToken = JWT.sign({ id, status: "user" });
+		const refreshToken = getRefreshToken(id);
+
+		req.id = id as unknown as string;
+
+		res.cookie("refreshToken", refreshToken, {
+			httpOnly: true,
+			secure: true,
+			maxAge: 7 * 24 * 60 * 60,
+		});
+
+		res.status(CREATED).json({ message: "user created!", accessToken });
 	} catch (error) {
 		logger.error(error);
 		res.status(INTERNAL_SERVER_ERROR).json({ error: "Error signing user up" });
@@ -38,11 +64,31 @@ export const signUp = async (req: GlobalRequest, res: GlobalResponse) => {
 };
 
 export const projectSignUp = async (req: GlobalRequest, res: GlobalResponse) => {
-	try {
+  try {
+    const logo = "logo-p";
 
-		res.status(CREATED).json({ message: "project created!" });
+    req.body.logo = logo;
+
+    const projectUser = await project.create(req.body);
+
+		const id = projectUser._id;
+
+		const accessToken = JWT.sign({ id, status: "project" });
+		const refreshToken = getRefreshToken(id);
+
+		req.id = id as unknown as string;
+
+		res.cookie("refreshToken", refreshToken, {
+			httpOnly: true,
+			secure: true,
+			maxAge: 30 * 24 * 60 * 60,
+		});
+
+		res.status(CREATED).json({ message: "project created!", accessToken });
 	} catch (error) {
 		logger.error(error);
-		res.status(INTERNAL_SERVER_ERROR).json({ error: "Error signing project up" });
+		res
+			.status(INTERNAL_SERVER_ERROR)
+			.json({ error: "Error signing project up" });
 	}
 };
