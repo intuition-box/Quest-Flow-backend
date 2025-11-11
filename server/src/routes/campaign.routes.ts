dfc45559d71e74ffbff668a341b22b8d2f6f557f
cakezero <sareahj.com@gmail.com>
2025-11-11 04:49:38 +0100

    routes update to allow scheduled campaign add campaign contract address

--- server/src/routes/campaign.routes.ts
+++ server/src/routes/campaign.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { claimCampaignRewards, closeCampaign, createCampaign, joinCampaign, updateCampaign } from "@/controllers/campaign.controller";
+import { addCampaignAddress, claimCampaignRewards, closeCampaign, createCampaign, joinCampaign, updateCampaign } from "@/controllers/campaign.controller";
 import { createCampaignTasks, createEcosystemTasks, fetchCampaignTasks } from "@/controllers/tasks.controller";
 import { authenticateProject, authenticateUser } from "@/middlewares/auth.middleware";
 
 const router = Router();
 
 router
-	.patch("/close-campaign", authenticateProject, closeCampaign)
+	.patch("/add-campaign-address", authenticateProject, addCampaignAddress)
 	.post("/complete-campaign", authenticateProject, claimCampaignRewards)
+	.patch("/close-campaign", authenticateProject, closeCampaign)
 	.post("/create-campaign", authenticateProject, createCampaign)
 	.post("/create-campaign-tasks", authenticateProject, createCampaignTasks)
 	.post("/create-ecosystem-tasks", authenticateProject, createEcosystemTasks)
