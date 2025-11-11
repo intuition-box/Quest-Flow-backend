b9bf7fe5a2b068209f4b8eceb85d049b8969c599
cakezero <sareahj.com@gmail.com>
2025-11-11 06:42:40 +0100

    edit: testnet configuration

--- server/src/utils/chain.utils.ts
+++ server/src/utils/chain.utils.ts
@@ -19,19 +19,19 @@ export const intuitionMainnet = defineChain({
 });
 
 export const intuitionTestnet = defineChain({
-	id: 1155,
-	name: "Intuition Mainnet",
+	id: 13579,
+	name: "Intuition Testnet",
 	nativeCurrency: {
 		decimals: 18,
-		name: "TRUST",
-		symbol: "TRUST",
+		name: "tTRUST",
+		symbol: "tTRUST",
 	},
 	rpcUrls: {
 		default: {
-			http: ["https://rpc.intuition.systems"],
+			http: ["https://testnet.rpc.intuition.systems"],
 		},
 	},
 	blockExplorers: {
-		default: { name: "Explorer", url: "https://explorer.intuition.systems" },
+		default: { name: "Explorer", url: "https://testnet.explorer.intuition.systems" },
 	},
 });
