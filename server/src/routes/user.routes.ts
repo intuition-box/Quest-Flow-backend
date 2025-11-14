aebd8ddb811aa229a81a9439c09eed4bd01056a8
cakezero <sareahj.com@gmail.com>
2025-11-14 23:07:26 +0100

    leaderboard and sign in route

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
-import { fetchUser, referralInfo, updateUsername } from "@/controllers/app.controller";
+import { fetchUser, getLeaderboard, referralInfo, updateUsername } from "@/controllers/app.controller";
 import { claimDailyQuest, claimOneTimeQuest } from "@/controllers/tasks.controller";
-import { signUp } from "@/controllers/auth.controller";
+import { signIn, signUp } from "@/controllers/auth.controller";
 import { authenticateUser } from "@/middlewares/auth.middleware";
 
 const router = Router();
 
 router
 	.post("/claim-daily-quest", authenticateUser, claimDailyQuest)
+	.get("/leaderboard", getLeaderboard)
 	.post("/claim-one-time-quest", authenticateUser, claimOneTimeQuest)
 	.get("/profile", authenticateUser, fetchUser)
 	.get("/referrerInfo", authenticateUser, referralInfo)
 	.post("/sign-up", signUp)
+	.post("/sign-in", signIn)
 	.patch("/update", authenticateUser, updateUsername);
 
 export default router;
